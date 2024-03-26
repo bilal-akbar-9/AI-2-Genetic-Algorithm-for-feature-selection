2512557caadbb9a5044155f381e67eb98710099b
Bilal Akbar <geo.bilal7@gmail.com>
2024-03-26 16:29:20 +0500

    Add intensity modality

--- DataExtractor.py
+++ DataExtractor.py
@@ -1,10 +1,10 @@
-import glob
 import os
 import pandas as pd
 
 dataset_path = "archive/"
 desired_modality = "01"
 desired_emotions = ["03", "04"]
+desired_intensity = "01"
 
 happy_data = pd.DataFrame()
 sad_data = pd.DataFrame()
@@ -16,11 +16,12 @@ for filename in csv_filenames:
     parts = filename.split("-")
     modality_code = parts[0]
     emotion_code = parts[2]
+    intensity_code = parts[3]
 
     # Check if the file matches the desired modality and emotion codes
-    if modality_code == str(desired_modality) and emotion_code in desired_emotions:
+    if modality_code == str(desired_modality) and emotion_code in desired_emotions and intensity_code == str(desired_intensity):
         # Construct the full file path
-        file_path = os.path.join(dataset_path, filename)
+        file_path = f"{dataset_path}/{filename}"
         # Read the CSV file from the specified path
         csv_data = pd.read_csv(file_path)
 
@@ -35,9 +36,12 @@ for filename in csv_filenames:
 print("The total number of happy data rows:", len(happy_data))
 print("The total number of sad data rows:", len(sad_data))
 
+# The data folder path
+data_folder_path = "data/"
+
 # Save the filtered data to new CSV files
-happy_data.to_csv("data/happy.csv", index=False)
-sad_data.to_csv("data/sad.csv", index=False)
+happy_data.to_csv(f"{data_folder_path}happy.csv", index=False)
+sad_data.to_csv(f"{data_folder_path}sad.csv", index=False)
 
 # print that the data extraction is done
 print("Data extraction is done!")
